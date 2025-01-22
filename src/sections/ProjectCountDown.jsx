ed222a4acacbb1b60e91b90f921e9b0d0dd06f02
NouranAlSharawneh <noransharawna@hotmail.com>
2025-01-22 18:05:53 +0300

    #

---
+++ src/sections/ProjectCountDown.jsx
@@ -0,0 +1,24 @@
+const ProjectCountDown = () => {
+  return (
+    <section>
+      <div>
+        <div>
+          <div className="title">
+            <h3>Projects +55</h3>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla,
+              accusamus. Veniam suscipit, rem cum blanditiis aspernatur dolor
+              iusto labore odit, quos accusantium iste maxime incidunt! Amet
+              officiis reiciendis minima ea.
+            </p>
+          </div>
+        </div>
+        <div>
+          <img src="../../public/assets/side.jpg" alt="" />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default ProjectCountDown;
