77608324a4ee0a87600fb4a891b31a71aad6225d
Nouran <noransharawna@hotmail.com>
2025-02-10 13:18:56 +0300

    Cleaning up the code

--- src/sections/Message.jsx
+++ src/sections/Message.jsx
@@ -1,22 +1,17 @@
 import { useEffect, useRef } from "react";
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
-import SplitType from "split-type"; // Utility for splitting text into words/letters
+import SplitType from "split-type";
 
 gsap.registerPlugin(ScrollTrigger);
 
 const Message = () => {
-  const textRef = useRef(null); // Reference for the text
-  const imgRef = useRef(null); // Reference for the image
-
+  const textRef = useRef(null);
   useEffect(() => {
     const textElement = textRef.current;
-    const imgElement = imgRef.current;
 
-    // Split the text into words
     const splitText = new SplitType(textElement, { types: "words" });
 
-    // Animate each word
     gsap.fromTo(
       splitText.words,
       { opacity: 0, y: 50 },
@@ -24,7 +19,7 @@ const Message = () => {
         opacity: 1,
         y: 0,
         duration: 0.8,
-        stagger: 0.1, // Delay between each word's animation
+        stagger: 0.1,
         ease: "power2.out",
         scrollTrigger: {
           trigger: textElement,
@@ -35,24 +30,6 @@ const Message = () => {
       }
     );
 
-    // Animate the image
-    gsap.fromTo(
-      imgElement,
-      { opacity: 0, scale: 0.8 },
-      {
-        opacity: 1,
-        scale: 1,
-        duration: 1.5,
-        scrollTrigger: {
-          trigger: imgElement,
-          start: "top 85%",
-          end: "bottom 25%",
-          scrub: 1,
-        },
-      }
-    );
-
-    // Cleanup SplitType on unmount
     return () => {
       splitText.revert();
     };
@@ -65,9 +42,6 @@ const Message = () => {
           I strive to design thoughtful and sustainable spaces that can serve
           and inspire future generations, blending architecture and interior
           design to create environments that stand the test of time.
-          {/* Their attention to details and ability to merge{" "}
-        <div className="message-img" ref={imgRef}></div> aesthetics with
-        functionality exceeded our expectations. */}
         </h4>
       </div>
     </section>
